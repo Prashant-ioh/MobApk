bbfc49ff44379142f5d238a77f9729eaf36b3791
Prashant-ioh <vikky4424@gmail.com>
2024-09-09 11:58:07 +0530

    Add project files.

---
+++ MobApk/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MobApk
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MobApk/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MobApk
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
